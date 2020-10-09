e6020158c07084fff81774a2f7e6998c5fa4bcbb
Cameron Manavian <cameron.manavian@gmail.com>
2020-10-09 11:41:01 -0700

    Redis cache rough

--- src/car/routes.rs
+++ src/car/routes.rs
@@ -1,13 +1,62 @@
 use crate::car::Car;
-use actix_web::{get, web, HttpResponse, Responder};
+use actix::prelude::*;
+use actix_redis::{Command, RedisActor};
+use actix_web::{error, get, web, Error, HttpResponse, Responder};
+use redis_async::{resp::RespValue, resp_array};
 use sqlx::mysql::MySqlPool;
 
 #[get("/cars")]
-async fn find_all(db_pool: web::Data<MySqlPool>) -> impl Responder {
+async fn find_all(
+    db_pool: web::Data<MySqlPool>,
+    redis: web::Data<Addr<RedisActor>>,
+) -> impl Responder {
+    let existing_cache = redis.send(Command(resp_array!["GET", "cars:all"])).await;
+    match existing_cache {
+        Err(e) => Err(Error::from(e)),
+        Ok(res) => match res {
+            Ok(val) => {
+                match val {
+                    RespValue::Error(err) => {
+                        return Err(error::ErrorInternalServerError(err));
+                    }
+                    RespValue::SimpleString(s) => {
+                        if let Ok(val) = serde_json::from_str(&s) {
+                            return Ok(Some((val, value)));
+                        }
+                    }
+                    RespValue::BulkString(s) => {
+                        if let Ok(val) = serde_json::from_slice(&s) {
+                            return Ok(Some((val, value)));
+                        }
+                    }
+                    _ => (),
+                }
+                Ok(None) =>
+            }
+            Err(err) => Err(error::ErrorInternalServerError(err)),
+        },
+    };
+
     let result = Car::find_all(db_pool.get_ref()).await;
     match result {
-        Ok(todos) => HttpResponse::Ok().json(todos),
-        _ => HttpResponse::BadRequest().body("Error trying to read all cars from database"),
+        Ok(cars) => {
+            let redis_string = serde_json::to_string(&cars).unwrap();
+            let save_result = redis
+                .send(Command(resp_array![
+                    "SET",
+                    "cars:all",
+                    redis_string,
+                    "EX",
+                    "300"
+                ]))
+                .await;
+
+            match save_result {
+                _ => HttpResponse::Ok().json(cars),
+            }
+        }
+
+        Err(_) => HttpResponse::BadRequest().body("Cars not found"),
     }
 }
 
@@ -15,7 +64,7 @@ async fn find_all(db_pool: web::Data<MySqlPool>) -> impl Responder {
 async fn find(id: web::Path<i32>, db_pool: web::Data<MySqlPool>) -> impl Responder {
     let result = Car::find_by_id(id.into_inner(), db_pool.get_ref()).await;
     match result {
-        Ok(todo) => HttpResponse::Ok().json(todo),
+        Ok(car) => HttpResponse::Ok().json(car),
         _ => HttpResponse::BadRequest().body("Car not found"),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@
 extern crate log;
 
 use actix_files::Files;
-use actix_web::{App, HttpServer};
+use actix_redis::RedisActor;
+use actix_web::{middleware, App, HttpServer};
 use anyhow::Result;
 use dotenv::dotenv;
+use meilisearch_sdk::client::*;
 use sqlx::mysql::MySqlPool;
 use std::env;
 
@@ -17,13 +19,21 @@ async fn main() -> Result<()> {
     env_logger::init();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let redis_url = env::var("REDIS_URL").expect("REDIS_URL is not set in .env file");
     let db_pool = MySqlPool::new(&database_url).await?;
     let host = env::var("HOST").expect("HOST is not set in .env file");
     let port = env::var("PORT").expect("PORT is not set in .env file");
 
+    let client = Client::new("http://localhost:7700", "masterKey");
+    info!("Indexing Search");
+    car::fixtures(client, &db_pool.clone()).await?;
+    info!("Search Ready");
+
     let server = HttpServer::new(move || {
         App::new()
             .data(db_pool.clone())
+            .data(RedisActor::start(redis_url.clone()))
+            .wrap(middleware::Logger::default())
             .configure(car::init)
             .service(Files::new("/", "./app/dist/public/").index_file("index.html"))
     });
